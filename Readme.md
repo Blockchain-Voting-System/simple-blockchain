0f7eeeabec0b1dfcd5ddd5462e2b860745d13532
Kacper Augustyn <kacperaugustyn@o2.pl>
2024-04-27 16:41:28 +0200

    serialization refactored, blockchain tests

--- Readme.md
+++ Readme.md
@@ -2,3 +2,11 @@
 
 Simple blockchain based on Proof of Work consensus mechanism written in Python.
 Nodes use TCP protocol to exchange data about current state of the chain.
+
+## Tests
+
+To run tests go to tests directory and run the following command:
+
+```console
+python3 -m unittest discover -s .
+```
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+from blockchain.blockchain import Block, Transaction
+import random
+
 class bcolors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -8,3 +11,14 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+def create_block():
+    block = Block()
+    for _ in range(2):
+        t = Transaction("1", "2", 100)
+        t.timestamp = "123"
+        block.add_transaction(t)
+    block.mine(2)
+    block.time_to_mine = 1.0
+    block.nonce = 132
+    return block
